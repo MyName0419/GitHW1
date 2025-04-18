ab73158f4e62f91862ef1f95989354e14fdb3cfa
Jinwoo Chang <jinwoo990419@gmail.com>
2025-04-18 23:09:09 +0900

    Hello World 3

--- main.py
+++ main.py
@@ -17,6 +17,6 @@ if __name__ == '__main__':
     print_hi('Hello World 2')
 
     # adding new item in hotfix
-    print_hi('Hello World 3')
+    print_hi('Hello World 3 ')
 
 # https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
